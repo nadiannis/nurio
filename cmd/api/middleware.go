46a24039d94d18963206e1b7f5b3dbb414af3309
nadiannis <annisanadianeyla@gmail.com>
2025-02-08 21:50:19 +0700

    chore: create middleware to recover panic

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -12,6 +13,20 @@ func (app *application) requestLogger(next http.Handler) http.Handler {
 			"method":      r.Method,
 			"url":         r.URL.RequestURI(),
 		})
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
 
 		next.ServeHTTP(w, r)
 	})
