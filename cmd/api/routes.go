517ccdba11f9659090efde6f192d4d5d0983e26a
nadiannis <annisanadianeyla@gmail.com>
2025-02-09 18:33:24 +0700

    chore: create middleware to share API responses with any other origin

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -7,5 +7,5 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("GET /api/v1/healthcheck", http.HandlerFunc(app.healthcheck))
 
-	return app.recoverPanic(app.requestLogger(mux))
+	return app.recoverPanic(app.enableCORS(app.requestLogger(mux)))
 }
